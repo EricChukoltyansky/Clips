1a946c00226d7dcd49b5c4ce332a192c58754524
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-16 14:00:14 +0300

    creating ID system for service modal

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  private visible = false;
+  private modals: IModal[] = [];
 
   constructor() {}
 
-  isModalOpen() : boolean {
-    return this.visible;
+  isModalOpen(): boolean {
+    return true;
   }
 
-  toggleModal() : void {
-    this.visible = !this.visible;
+  toggleModal(): void {
+    // this.visible = !this.visible;
+  }
+
+  register(id: string): void {
+    this.modals.push({
+      id,
+      visible: false,
+    });
   }
 }

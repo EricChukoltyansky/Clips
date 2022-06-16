cedc7ff93791f4d9c74185e28fed5ea75320dc00
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-16 17:59:25 +0300

    memory leaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./custom.css']
+  styleUrls: ['./custom.css'],
 })
 export class AppComponent {
-  title = 'clips';
+  showModal: boolean = true;
+
+  constructor(public modal: ModalService) {}
+
+  ngOnInit() {
+    setInterval(() => {
+      this.showModal = !this.showModal;
+      console.log(this.modal.modals);
+    }, 1000);
+  }
 }
--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -9,7 +9,7 @@ interface IModal {
   providedIn: 'root',
 })
 export class ModalService {
-  private modals: IModal[] = [];
+  public modals: IModal[] = [];
 
   constructor() {}
 

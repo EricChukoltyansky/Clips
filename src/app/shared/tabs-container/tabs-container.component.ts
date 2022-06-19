c941c62fb383eba84cce2d47b4652672c6d1c696
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-19 10:16:23 +0300

    contentChildren decorator

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChildren } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.css']
+  styleUrls: ['./tabs-container.component.css'],
 })
 export class TabsContainerComponent implements OnInit {
+  @ContentChildren(TabComponent) tabs = {};
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.tabs);
   }
-
 }

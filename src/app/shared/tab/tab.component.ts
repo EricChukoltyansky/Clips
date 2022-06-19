a412ea0640f9356364c2f79f2df60cf76615cbe2
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-19 13:38:35 +0300

    Preventing default behviour of anchor

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TabComponent implements OnInit {
   @Input() tabTitle: string = '';
+  @Input() active: boolean = false;
 
   constructor() {}
 

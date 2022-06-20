4bd4318a389a0b609616cd17ff0d0b116ba727a4
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-20 11:01:01 +0300

    validation forms

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,9 +1,26 @@
-import { Component} from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(3),
+    ]),
+    age: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [Validators.required]),
+  });
+}

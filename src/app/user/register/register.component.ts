b91a5732eda0d62ed1b03ea55863ae57fdbf2ee5
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-20 15:43:04 +0300

    binding form controlls

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,20 +7,27 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  registerForm: FormGroup = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(20),
-    ]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.email,
-      Validators.minLength(3),
-    ]),
-    age: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required]),
-    phoneNumber: new FormControl('', [Validators.required]),
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(20),
+  ]);
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email,
+    Validators.minLength(3),
+  ]);
+  age = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+  confirmPassword = new FormControl('', [Validators.required]);
+  phoneNumber = new FormControl('', [Validators.required]);
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber,
   });
 }

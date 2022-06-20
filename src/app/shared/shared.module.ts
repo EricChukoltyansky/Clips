4185e9af90c88a4d52b3667341547c9e3a282f7e
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-20 13:36:38 +0300

    creating input element

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
 // import { ModalService } from '../services/modal.service';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
   imports: [CommonModule],
   exports: [ModalComponent, TabsContainerComponent, TabComponent],
   // providers: [ModalService],

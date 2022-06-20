ff1098ec346ba612df022431c85ac5e023e2534d
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-20 15:59:29 +0300

    refactoring input element in the form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 // import { ModalService } from '../services/modal.service';
 
 @NgModule({
@@ -13,7 +14,7 @@ import { InputComponent } from './input/input.component';
     TabComponent,
     InputComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [
     ModalComponent,
     TabsContainerComponent,

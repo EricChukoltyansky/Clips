aaf89c3be1e3c04b556b6a0690d1315afeb17eed
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-29 10:33:13 +0300

    pluck operator

--- rxjs/src/index.js
+++ rxjs/src/index.js
@@ -1,7 +1,7 @@
 import { interval, fromEvent, of, from } from "rxjs";
 import { map, filter, scan, switchMap } from "rxjs/operators";
 
-const observable = fromEvent(document, "keydown").pipe(map((e) => e.code));
+const observable = fromEvent(document, "keydown").pipe(pluck("code"));
 
 const subscription = observable.subscribe({
   next(value) {

71b99b9e87e4d3dfa3fb59742d652d8b5a7bb197
EricChukoltyansky <kazohelmet@gmail.com>
2022-06-29 21:01:59 +0300

    ngModel forms

--- rxjs/src/index.js
+++ rxjs/src/index.js
@@ -1,7 +1,18 @@
 import { interval, fromEvent, of, from } from "rxjs";
-import { map, filter, scan, switchMap, pluck, filter } from "rxjs/operators";
+import {
+  map,
+  filter,
+  scan,
+  switchMap,
+  pluck,
+  filter,
+  tap,
+  mergeMap,
+} from "rxjs/operators";
 
-const observable = interval(500).pipe(take(),reduce((acc, curr) => acc + curr, 0));
+const observable = fromEvent(button, "click").pipe(
+  mergeMap(() => from(fetch("https://jsonplaceholder.typicode.com/todos/1")))
+);
 
 const subscription = observable.subscribe({
   next(value) {

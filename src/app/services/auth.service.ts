f94394f35058ee056e749b49d4e793263be7d01c
EricChukoltyansky <kazohelmet@gmail.com>
2022-07-04 14:47:20 +0300

    delay operator

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,7 +5,7 @@ import {
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 import IUser from '../models/user.models';
 
 @Injectable({
@@ -14,10 +14,12 @@ import IUser from '../models/user.models';
 export class AuthService {
   private userCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.userCollection = db.collection<IUser>('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
   }
 
   public async createUser(userData: IUser) {

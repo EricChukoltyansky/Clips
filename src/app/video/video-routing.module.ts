4ebbd5a6f2fde20df4521b3a3fd77c0db44c248b
EricChukoltyansky <kazohelmet@gmail.com>
2022-08-16 15:08:52 +0300

    navigate to manage

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ManageComponent } from './manage/manage.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'manage', component: ManageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class VideoRoutingModule { }
+export class VideoRoutingModule {}
